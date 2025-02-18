88fef3f2999e4062dd98cbf90f6ec21102a31caa
DeveloperUno <119540576+DeveloperUno2024@users.noreply.github.com>
2025-02-18 15:03:15 -0500

    first commit

---
+++ DecisionesYBucles/DecisionesYBucles/Program.cs
@@ -0,0 +1,399 @@
+﻿// Lógica Booleana
+
+// Descisiones e iteraciones
+
+var cantidad1 = 5;
+var cantidad2 = 5;
+
+var cantidadIgualACantidad2 = cantidad1 == cantidad2; // true, porque 5 es igual a 5
+var cantidadDiferenteDeCantidad2 = cantidad1 != cantidad2; // false, porque 5 no es diferente de 5
+Console.WriteLine($"Son {cantidad1} y {cantidad2} iguales?:  {cantidadIgualACantidad2}");
+Console.WriteLine($"Son {cantidad1} y {cantidad2} diferentes?:  {cantidadDiferenteDeCantidad2}");
+
+// Operadores de comparación, mayor que.
+// Menor que = <
+// Mayor que = >
+// Menor o igual que = <=
+// Mayor o igual que = >=
+
+var esCantidad1MenorQue10 = cantidad1 < 10; // true, porque 5 es menor que 10
+var esCantidad1MayorQue10 = cantidad1 > 10; // false, porque 5 no es mayor que 10
+
+var esCantidad1MenorOIgualQue10 = cantidad1 <= 10; // true, porque 5 es menor que 10
+var esCantidad1MayorOIgualQue10 = cantidad1 >= 10; // false, porque 5 no es mayor ni igual que 10
+
+// Con strings, los operadores de comparación funcionan de la misma manera
+var nombre1 = "Juan";
+var nombre2 = "Pedro";
+
+var sonLosNombresIguales = nombre1 == nombre2; // false, porque Juan no es igual a Pedro
+var sonLosNombresDiferentes = nombre1 != nombre2; // true, porque Juan es diferente a Pedro
+
+// En versiones mas recientes de C# NO se recomienda usar == para verificar si algo es nullo, se prefiere usar is null
+var esNombre1Nulo = nombre1 is null; // false, porque nombre1 no es nulo
+var esNombre1NoNulo = nombre1 is not null; // true, porque nombre1 no es nulo
+
+Console.WriteLine($"Es {nombre1} igual a nulo?: " + esNombre1Nulo);
+Console.WriteLine($"Es {nombre1} diferente a nulo?: " + esNombre1NoNulo);
+
+// Operadores condicionales AND y OR (Booleanos)
+// AND = &&
+// && es verdadero si ambos operandos son verdaderos
+
+// true && true = true
+// true && false = false
+// false && true = false
+// false && false = false
+var cantidad3 = 6;
+var estaEntre5y10 = cantidad3 > 5 && cantidad3 < 10; // true, porque 6 es mayor que 5 y menor que 10
+Console.WriteLine($"Es {cantidad3} mayor que 5 y menor que 10?: " + estaEntre5y10);
+
+// OR = ||
+// || es verdadero si al menos uno de los operandos es verdadero
+// alt + 124 = |
+
+// true || true = true
+// true || false = true
+// false || true = true
+// false || false = false
+var cantidad1Es5o6 = cantidad3 == 5 || cantidad3 == 6; // true, porque 6 es igual a 6
+Console.WriteLine($"Es {cantidad3} igual a 5 o 6?: " + cantidad1Es5o6);
+
+// Error de referencia nula
+// Si intentamos acceder a una propiedad de un objeto nulo, se lanza una excepción de referencia nula
+// Para evitar esto, podemos usar el operador de nulabilidad ?.
+string? apellido = null;
+var estaElApellidoEnMayuscula = apellido is not null && apellido == apellido.ToUpper(); // true, porque Espitia es igual a Espitia
+Console.WriteLine($"Esta el apellido en mayuscula?: " + estaElApellidoEnMayuscula);
+
+
+// Condicional IF
+// if (condicion)
+// Se ejecuta si la condición es verdadera
+
+var cantidad4 = 6;
+if (cantidad4 > 5)
+{
+    Console.WriteLine($"{cantidad4} es mayor que 5");
+} else if (cantidad4 == 3)
+{
+    Console.WriteLine("El valor es 3!");
+}
+else
+{
+    Console.WriteLine("Es menor o igual a 5");
+}
+
+Console.WriteLine("Fin del programa");
+
+
+// Operador ternario
+
+bool usuarioEstaLogueado = false;
+
+// Forma tradicional
+// Se usa para asignar valores a variables
+//string mensajeBienvenida;
+//if (usuarioEstaLogueado)
+//{
+//    mensajeBienvenida = "¡Que bueno que has vuelto!";
+//} else
+//{
+//    mensajeBienvenida = "Logueate para comenzar";
+//}
+
+// Forma corta
+// Solo se puede usar para asignar valores a variables
+// <condicion> ? <si la condicion es true> : <si la condicion es false>
+string mensajeBienvenida = usuarioEstaLogueado ? "¡Que bueno que has vuelto!" : "Logueate para comenzar";
+ 
+Console.WriteLine(mensajeBienvenida);
+
+// Sentencia Switch
+// Se recomienda utilizar switch cuando la comparacion la hacemos siempre sobre un mismo valor.
+
+var lugar = 4;
+
+// Esto es funcional, pero no es la mejor práctica
+//if (lugar == 1)
+//{
+//    Console.WriteLine("Primer lugar");
+//}
+//else if (lugar == 2)
+//{
+//    Console.WriteLine("Segundo lugar");
+//}
+//else if (lugar == 3)
+//{
+//    Console.WriteLine("Tercer lugar");
+//}
+//else
+//{
+//    Console.WriteLine("No es un lugar valido");
+//}
+
+// Mejor práctica
+switch (lugar)
+{
+    case 1:
+        Console.WriteLine("Primer lugar");
+        break;
+    case 2:
+        Console.WriteLine("Segundo lugar");
+        break;
+    case 3:
+        Console.WriteLine("Tercer lugar");
+        break;
+    case 4:
+    case 5:
+        Console.WriteLine("Cuarto o quinto lugar");
+        break;
+    default:
+        // Se ejecuta si no se cumple ningun caso
+        Console.WriteLine("No es un lugar valido");
+        break;
+}
+
+
+// Patrones relacionales
+
+var temperatura = 37;
+
+var mensaje = temperatura switch
+{
+    37 => "No tienes ninguna condicion",
+    > 37 => "Tienes fiebre",
+    < 37 => "Estas por debajo de la temperatura normal"
+};
+
+Console.WriteLine(mensaje);
+
+// Patrones logicos
+
+// Ejemplo 1: Not
+string? apellido1 = null;
+if (apellido1 is not null)
+{
+    Console.WriteLine(apellido1.ToUpper());
+}
+
+Console.WriteLine("Fin");
+
+// Ejemplo 2: And
+// parmashing, es una tecnica que se usa para pasar parametros a un metodo
+// No se usa el operador && porque no es un operador de comparacion, se usa la palabra and
+var temperatura1 = 41;
+var mensaje1 = temperatura1 switch
+{
+    37 => "No tienes ninguna condicion",
+    > 37 and < 39 => "Tienes fiebre",
+    >= 39 and < 43 => "Tienes fiebre alta",
+    >= 43 => "Estas en peligro",
+    < 37 => "Estas por debajo de la temperatura normal"
+};
+
+Console.WriteLine(mensaje1);
+
+// Ejemplo 3: Or
+// _ es un comodin que se usa para cualquier otro caso
+
+var hoy = DateTime.Today;
+var estacion = hoy.Month switch
+{
+    3 or 4 or 5 => "Primavera",
+    6 or 7 or 8 => "Verano",
+    9 or 10 or 11 => "Otoño",
+    12 or 1 or 2 => "Invierno",
+    _ => throw new ApplicationException("Mes invalido")
+};
+
+Console.WriteLine(estacion);
+
+// Expresiones Switch y Discard 
+
+var lugar1 = 1;
+
+// Esto es funcional, pero no es la mejor práctica
+// Opcion 1, usando switch
+switch (lugar1)
+{
+    case 1:
+        Console.WriteLine("Primer lugar");
+        break;
+    case 2:
+        Console.WriteLine("Segundo lugar");
+        break;
+    case 3:
+        Console.WriteLine("Tercer lugar");
+        break;
+    case 4:
+    case 5:
+        Console.WriteLine("Cuarto o quinto lugar");
+        break;
+    default:
+        // Se ejecuta si no se cumple ningun caso
+        Console.WriteLine("No es un lugar valido");
+        break;
+}
+
+// Discard "_" indicar que una variable no se va a usar
+// Opcion 2, usando switch y discard
+var mensaje2 = lugar switch
+{
+    1 => "Primer lugar",
+    2 => "Segundo lugar",
+    3 => "Tercer lugar",
+    4 or 5 => "Cuarto o quinto lugar",
+    _ => "No es un lugar valido"
+};
+
+Console.WriteLine(mensaje2);
+
+
+
+// Bluce While
+// Se ejecuta mientras la condicion sea verdadera
+
+var contador = 1;
+// while, significa mientras
+
+// si la condicion while es falsa, el bucle no se ejecuta
+while (contador <= 10)
+{
+    Console.WriteLine(contador);
+    contador++; // contador = contador + 1
+}
+
+var balance = 200m;
+var interes = 1.07m;
+var contadorInteres = 1;
+while (contadorInteres <= 20)
+{
+    balance *= interes;
+    contadorInteres++;
+}
+
+Console.WriteLine($"Balance en el año {contadorInteres}: {balance}");
+
+
+// Bucle Do While
+// Se ejecuta al menos una vez, y luego se ejecuta mientras la condicion sea verdadera
+// La diferencia con el while es que el do while se ejecuta al menos una vez
+
+var contador2 = 1;
+//while (contador2 > 10)
+//{
+//    Console.WriteLine(contador2);
+//    contador++; // contador2 = contador2 + 1
+//}
+
+// Si la condicion es falsa, el bucle se ejecuta al menos una vez
+do
+{
+    Console.WriteLine(contador2);
+    contador2++; // contador2 = contador2 + 1
+} while (contador2 > 10);
+
+Console.WriteLine("Fin del programa");
+
+// Bucle For
+// Se ejecuta un numero determinado de veces
+
+// La diferencia entre el while y el for es que el for tiene la inicializacion, la condicion y la modificacion en una sola linea
+// Si yo quiero usar la variable contador2 en otro lugar, debo declararla fuera del bucle
+
+//int contador3 = 1; // Inicializacion
+//while (contador3 <= 10) // Condicion
+//{ // Bloque de codigo
+//    Console.WriteLine(contador3);
+//    contador3++; // Modificacion de la variable inicializada
+//}
+
+// for (inicializacion; condicion; modificacion)
+for (int contador4 = 1; contador4 <= 10; contador4++)
+{
+    Console.WriteLine(contador4);
+}
+
+
+// Bucle For Each (foreach)
+// Se usa para recorrer colecciones de datos
+// Nos permite iterar sobre cada elemento de una coleccion
+
+// foreach = para cada
+var nombre3 = "Andres";
+foreach (var letra in nombre3)
+{
+    Console.WriteLine(letra); // Letra = A, n, d, r, e, s
+}
+
+
+// Sentencias de Saltos Break y Continue
+for (int i = 1; i <= 10; i++)
+{
+    if (i == 5)
+    {
+        break; // Sale del bucle, no se ejecuta el resto del bucle, llegaria hasta el 4
+        //continue; // Salta la iteracion actual, no se ejecuta el resto del bloque, llegaria hasta el 10
+    } 
+
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("Fin del programa");
+
+
+// Bucle infinito
+
+//while (true)
+//{
+//    Console.WriteLine("Este codigo no se detiene ");
+//}
+
+//for (; ; )
+//{
+//    Console.WriteLine("Este codigo no se detiene ");
+//}
+
+while (true)
+{
+    Console.WriteLine("Te damos la bienvenida a la calculadora de suma con C#");
+    Console.Write("Por favor, ingresa el primer numero: ");
+    var entradaUsuario = Console.ReadLine();
+    if (entradaUsuario is null)
+    {
+        Console.WriteLine("No ingresaste ningun valor");
+        continue;
+    }
+    var numero1 = decimal.Parse(entradaUsuario); // Podria fallar si el usuario ingresa un valor no numerico
+    Console.Write("Por favor, ingresa el segundo numero: ");
+    entradaUsuario = Console.ReadLine();
+    if (entradaUsuario is null)
+    {
+        Console.WriteLine("No ingresaste ningun valor");
+        continue;
+    }
+    var numero2 = decimal.Parse(entradaUsuario);
+    Console.WriteLine($"El resultado de la suma de {numero1} y {numero2} es: {numero1 + numero2}");
+    Console.Write("Deseas hacer otra suma? (y/n): ");
+    entradaUsuario = Console.ReadLine();
+    if (entradaUsuario != "y")
+    {
+        break;
+    }
+}
+
+Console.WriteLine("Gracias por usar la calculadora de suma con C#");
+
+
+
+// RESUMEN
+
+// 1. logica booleana, operadores de comparacion, operadores logicos, if, switch, operador ternario
+// 2. Operadores condicionales AND y OR (Booleanos)
+// 3. && es verdadero si ambos operandos son verdaderos
+// 4. || es verdadero si al menos uno de los operandos es verdadero
+// 5. Sentencias de seleccion: if, switch
+// 6. Bucles: while, do while, for, foreach
+// 7. Sentencias de saltos: break, continue
+// 8. Bucles infinitos
---
+++ Variables/Program.cs
@@ -0,0 +1,358 @@
+﻿// Las variables nos permiten almacenar información en la memoria de la computadora,
+// para poder utilizarla en cualquier momento de la ejecución del programa.
+
+// Console.WriteLine es un método que se usa para imprimir en la consola.
+// Para escribir en consola se usa Console.WriteLine, y dentro de los paréntesis se coloca el
+// texto que se quiere imprimir.
+
+Console.WriteLine("Teoria y ejemplos de Variables");
+
+// Variable de tipo int, se usa para almacenar números enteros.
+int alturanEnCentimetros;
+alturanEnCentimetros = 180;
+Console.WriteLine("La altura en centimetros es: " + alturanEnCentimetros);
+
+int edadDeAndres = 25;
+Console.WriteLine("La edad de Andres es: " + edadDeAndres);
+
+int distanciaDeLaTierraAlSolEnKilometros = 150_000_000; // Se pueden usar guiones bajos para separar los dígitos.
+Console.WriteLine("Distancia de la tierra en kilometros es: " + distanciaDeLaTierraAlSolEnKilometros);
+
+// int.MaxValue es una constante que contiene el valor máximo que puede almacenar una variable de tipo int.
+int numeroSuperGrande = int.MaxValue;
+Console.WriteLine("Numero maximo para la variable int : " + numeroSuperGrande);
+
+// uint es un tipo de dato que se usa para almacenar números enteros positivos.
+uint unint2 = 3_000_000_000; // 0 a hasta mas de 4 mil millones
+Console.WriteLine("Ejemplo de unint2: " + unint2);
+
+// long es un tipo de dato que se usa para almacenar números enteros que van desde -9,223,372,036,854,775,808 hasta 9,223,372,036,854,775,807.
+
+long long1 = 8_000_000_000_000;
+Console.WriteLine("Ejemplo de long1: " + long1);
+
+// ulong es un tipo de dato que se usa para almacenar números enteros positivos que van desde 0 hasta 18,446,744,073,709,551,615.
+
+ulong ulong1 = 18_000_000_000_000;
+Console.WriteLine("Ejemplo de ulong1: " + ulong1);
+
+// byte es un tipo de dato que se usa para almacenar números enteros positivos que van desde 0 hasta 255.
+byte byt1 = 145;
+Console.WriteLine("Ejemplo de byte: " + byt1);
+
+// sbyte es un tipo de dato que se usa para almacenar números enteros que van desde -128 hasta 127.
+sbyte sbyt1 = 120;
+Console.WriteLine("Ejemplo de sbyte: " + sbyt1);
+
+// short es un tipo de dato que se usa para almacenar números enteros que van desde -32,768 hasta 32,767.
+short short1 = 30_000;
+Console.WriteLine("Ejemplo de short: " + short1);
+
+// unshort es un tipo de dato que se usa para almacenar números enteros que van desde 0 hasta 65,535.
+ushort ushort1 = 60_000;
+Console.WriteLine("Ejemplo de ushort1: " + ushort1);
+
+// float es un tipo de dato que se usa para almacenar números decimales con una precisión de 7 dígitos.
+float altura = 180.123456789f; // F es un sufijo que se usa para indicar que el número es de tipo float.
+Console.WriteLine("La altura es: " + altura);
+
+// double es un tipo de dato que se usa para almacenar números decimales con una precisión de 15 dígitos.
+double anchura = 45.99; // D es un sufijo que se usa para indicar que el número es de tipo double.
+Console.WriteLine("La anchura es: " + anchura);
+
+// decimal es un tipo de dato que se usa para almacenar números decimales con una precisión de 28 dígitos.
+decimal monto = 9.99m; // M es un sufijo que se usa para indicar que el número es de tipo decimal. La precision es de 28 digitos a 29 digitos. Se recomienda para temas de dinero.
+Console.WriteLine("El monto es: " + monto);
+
+// bool es un tipo de dato que se usa para almacenar valores de verdadero o falso.
+bool soyFelipe = true;
+bool dosMasDosEsCinco = false;
+Console.WriteLine("Soy Felipe: " + soyFelipe);
+Console.WriteLine("Dos mas dos es cinco: " + dosMasDosEsCinco);
+
+// char es un tipo de dato que se usa para almacenar un solo carácter.
+char primeraLetraDeABC = 'A'; // Se usa comillas simples para indicar que es un solo carácter.
+Console.WriteLine("La primera letra de ABC es: " + primeraLetraDeABC);
+
+// string es un tipo de dato que se usa para almacenar texto.
+string Nombre = "Andres Espitia"; // Se usa comillas dobles para indicar que es un texto.
+Console.WriteLine("El nombre de Andres es: " + Nombre);
+
+// \ es un caracter de escape que se usa para indicar que el siguiente caracter es especial.
+string text = "Mi amigo me dijo una vez: \"Me caes bien\"";
+Console.WriteLine("Ejemplo de \\: ", text);
+
+// @ es un caracter de escape que se usa para indicar que el texto se va a imprimir tal cual está escrito.
+string carta = @"A quien pueda interesar, 
+
+Estimado señor, debe usted saber que...
+
+Atentamente, Andrés Espitia
+
+";
+Console.WriteLine("Ejemplo de texto con espacios de lineas: " + carta);
+
+// @ se usa para indicar que el texto se va a imprimir tal cual está escrito.
+string directorioDelArchivo = @"C:\Users\Andres\Documents\Proyectos";
+Console.WriteLine("Ejemplo del @: " + directorioDelArchivo);
+
+// Datetime es un tipo de dato que se usa para almacenar fechas y horas.
+DateTime ahora = DateTime.Now;
+Console.WriteLine("La fecha y hora actual es: " + ahora);
+
+// Espesificar una fecha y hora.
+DateTime primerDiaDelAño = new DateTime(2025, 1, 1, 2, 15, 23);
+Console.WriteLine("El primer día del año 2025 y hora es: " + primerDiaDelAño);
+
+// .Now.AddDays(1) se usa para sumar un día a la fecha actual.
+DateTime mañana = DateTime.Now.AddDays(1);
+Console.WriteLine("Mañana es: " + mañana);
+Console.WriteLine("El dia de mañana es: " + mañana.Day);
+Console.WriteLine("El mes es: " + mañana.Month);
+Console.WriteLine("El año es: " + mañana.Year);
+Console.WriteLine("El dia de la semana es: " + mañana.DayOfWeek);
+
+
+// Reglas para las variables
+// int 1Edad = 25; // No se puede empezar con un número.
+// se puede usar guion bajo para separar palabras.
+// int edad_de_andres = 25;
+// int edadDeAndres = 25; // Camel Case, significa que la primera letra de la primera palabra es minúscula y la primera letra de las siguientes palabras es mayúscula.
+// int EdadDeAndres = 25; // Pascal Case, significa que la primera letra de cada palabra es mayúscula.
+// No usar - en las variables.
+// int edad-de-andres = 25; // No se puede usar guion medio.
+
+
+// Variables implicitas tipadas
+// var se usa para declarar una variable sin especificar el tipo de dato.
+var apellido = "Espitia";
+Console.WriteLine("El apellido de Andres es: " + apellido);
+var hoy = DateTime.Now;
+Console.WriteLine("La fecha y hora actual es: " + hoy);
+var float1 = 3.14;
+Console.WriteLine("El valor de float1 es: " + float1);
+
+// No se puede cambiar el tipo de dato de una variable implicita.
+// var edad = 25;
+// edad = "25"; // No se puede cambiar el tipo de dato de una variable implicita.
+
+// Valores por defectos de distintos tipos de datos
+ int edad = default;
+ Console.WriteLine("La edad es: " + edad);
+ edad = 8;
+
+bool hoyEsViernes = default;
+Console.WriteLine("Valor por defecto de un int: " + hoyEsViernes);
+
+// El valor por defecto de default es NULL 
+
+// Tipos de valor y de referencia
+// Los tipos de valor almacenan el valor directamente en la memoria.
+// stack: es una parte de la memoria que se usa para almacenar variables locales y parámetros de métodos.
+// Los tipos de referencia almacenan una referencia a la memoria donde se encuentra el valor.
+// heap: es una parte de la memoria que se usa para almacenar objetos y arreglos.
+// Las variables de tipo de valor siempre contienen un valor.
+// Las variables de tipo de referencia podrian no contener referencia alguna.
+// Esto se expresa con null(nulo).
+// Si una variable es igual a null, significa que no contiene ninguna referencia a la memoria.
+// Es un tipo de referencia.
+// No contiene una referencia a algun valor en el heap.
+// String es un tipo de referencia.
+// Ejemplo de tipos de valor: int, decimal, double, float, bool, char, datetime.
+
+// Ejemplo de tipos de valor: int, decimal, double, float, bool, char, datetime.
+// int? es un tipo de dato que se usa para almacenar números enteros que pueden ser nulos.
+// ? es un sufijo que se usa para indicar que la variable puede ser nula.
+int? peso = null;
+Console.WriteLine("El valor por defecto de un int: " + peso);
+
+// Ejemplo de tipos de referencia: string, object, array, class, interface.
+//string? nombre = null;
+//Console.WriteLine("El valor por defecto de un string: " + nombre.ToUpper());
+
+
+// Expresiones - Operaciones binarias
+// Operadores aritméticos: +, -, *, /, %.
+// Operadores de comparación: ==, !=, >, <, >=, <=.
+// Operadores lógicos: &&, ||, !.
+// Operadores de asignación: =, +=, -=, *=, /=, %=.
+// Operadores de incremento y decremento: ++, --.
+
+// Operadores de concatenación: +.
+var cantidad1 = 10;
+var cantidad2 = 20;
+var resultado = cantidad1 + cantidad2;
+Console.WriteLine("El resultado de la suma de " + cantidad1 + " y " + cantidad2 +"  es:  " + resultado);
+
+// Concatenar texto, se usa el operador + para concatenar texto.
+var nombre = "Andres";
+var apellido1 = "Espitia";
+var resultadoString = nombre + apellido1;
+Console.WriteLine("El resultado de la concatenación es: " + resultadoString);
+
+// Operadores aritméticos - Suma, resta, multiplicación, división y residuo.
+// +, -, *, /, %.
+var cantidad3 = 7.1M;
+var cantidad4 = 3.5M;
+var resultadoSuma = cantidad3 + cantidad4;
+var resultadoResta = cantidad3 - cantidad4;
+var resultadoMultiplicacion = cantidad3 * cantidad4;
+// Se usa (double) para convertir el resultado a un número decimal.
+// Casteo, es la conversión de un tipo de dato a otro.
+//var resultadoDivision = (double)cantidad3 / cantidad4; 
+var resultadoDivision = cantidad3 / cantidad4;
+var resultadoResiduo = cantidad3 % cantidad4;
+
+Console.WriteLine("El resultado de la suma es: " + resultadoSuma);
+Console.WriteLine("El resultado de la resta es: " + resultadoResta);
+Console.WriteLine("El resultado de la multiplicación es: " + resultadoMultiplicacion);
+Console.WriteLine("El resultado de la división es: " + resultadoDivision);
+Console.WriteLine("El resultado del residuo es: " + resultadoResiduo);
+
+// Overflow Aritmetico
+// Cuando el resultado de una operación aritmética es mayor que el valor máximo que puede almacenar una variable, se produce un overflow aritmético.
+// En C# se puede usar la palabra clave checked para detectar un overflow aritmético.
+// El unchecked se usa para desactivar la detección de un overflow aritmético.
+
+//checked
+//{
+//    var maximo1 = int.MaxValue;
+//    var suma1 = maximo1 + 1;
+//    Console.WriteLine("El valor máximo de un int es: " + maximo1);
+//    Console.WriteLine("El resultado de la suma es: " + suma1);
+//}
+
+// checked
+// Este codigo genera una excepcion de tipo System.OverflowException
+// Para evitar que se genere la excepcion se puede usar la palabra clave unchecked.
+var maximo = int.MaxValue;
+var suma = unchecked(maximo + 1);
+Console.WriteLine(maximo);
+Console.WriteLine(suma);
+
+// Interpolacion de strings
+// Se usa el signo $ antes de las comillas para indicar que se va a interpolar un string.
+var cantidad5 = 7;
+var cantidad6 = 4;
+var resultado1 = cantidad5 + cantidad6;
+var nombre2 = "Andres";
+// Opcion 1, concatenar con el signo +
+Console.WriteLine("El resultado de la suma de " + cantidad1 + " y " + cantidad2 + "  es:  " + resultado);
+// Opcion 2, interpolacion usando el signo $
+Console.WriteLine($"El resultado de la suma de {cantidad5} y {cantidad6} es: {resultado1}");
+// Opcion 3, interpolacion usando el signo $ usando string
+Console.WriteLine($"El nombre de Andres en mayuscula es : {nombre2.ToUpper()}");
+
+// Operaciones Unarias
+// Operadores de incremento y decremento: ++, --.
+var cantidad7 = 7;
+var cantidad8 = 5;
+var cantidadPorMenos1 = -cantidad7; // (-1) * cantidad7
+//Console.WriteLine("El resultado de la cantidad por menos 1 es: " + cantidadPorMenos1);
+//cantidad7++; // cantidad7 = cantidad7 + 1
+//cantidad7--; // cantidad7 = cantidad7 - 1
+//Console.WriteLine("El resultado de la cantidad7++ es: " + cantidad7);
+//Console.WriteLine("El resultado de la cantidad7-- es: " + cantidad7);
+//++cantidad7; // cantidad7 = cantidad7 + 1
+//--cantidad7; // cantidad7 = cantidad7 - 1
+//Console.WriteLine("El resultado de la ++cantidad7 es: " + cantidad7);
+//Console.WriteLine("El resultado de la --cantidad7 es: " + cantidad7);
+
+// Si el ++ esta por delante de la variable, primero se incrementa la variable y luego se usa el valor.
+// Si el ++ esta por detras de la variable, primero se usa el valor de la variable y luego se incrementa la variable.
+var suma2 = cantidad7 + cantidad8; // cantidad7 = cantidad7 + 1, cantidad7 + cantidad8 // 8 + 5
+Console.WriteLine("El resultado de la suma es: " + suma2);
+
+
+// Expresiones Void
+// Las expresiones void no devuelven ningún valor.
+
+// Operadores de asignación: =, +=, -=, *=, /=, %=.
+var cantidad9 = 7m;
+var cantidad10 = 5m;
+// No se altera la cantidad9
+var suma3 = cantidad9 + cantidad10;
+/*cantidad7 += cantidad8;*/ // cantidad7 = cantidad7 + cantidad8
+//Console.WriteLine("Cantidad9 es: " + cantidad9);
+
+//cantidad9 -= cantidad10; // cantidad9 = cantidad9 - cantidad10
+//Console.WriteLine("Cantidad9 es: " + cantidad9);
+
+//cantidad9 *= cantidad10; // cantidad9 = cantidad9 * cantidad10
+//Console.WriteLine("Cantidad9 es: " + cantidad9);
+
+//cantidad9 /= cantidad10; // cantidad9 = cantidad9 / cantidad10
+//Console.WriteLine("Cantidad9 es: " + cantidad9);
+
+cantidad9 %= cantidad10; // cantidad9 = cantidad9 % cantidad10
+Console.WriteLine("Cantidad9 es: " + cantidad9);
+
+var nombre3 = "Andres";
+var apellido2 = " Espitia";
+
+nombre3 += apellido2; // nombre3 = nombre3 + apellido2
+Console.WriteLine("El nombre completo es: " + nombre3);
+
+// Ordenes de procedencia de las operaciones
+var cantidad11 = 1;
+var cantidad12 = 2;
+var cantidad13 = 3;
+var cantidad14 = cantidad11 + cantidad12 + cantidad13; // 6
+var cantidad15 = cantidad11 + cantidad12 * cantidad13; // 7
+var cantidad16 = (cantidad11 + cantidad12) * cantidad13; // 9   
+
+// No hacer esto, es confuso.
+var cantidad17 = (++cantidad11 + cantidad12++) * (cantidad13 + --cantidad13); // ??
+
+// Lo mejor es separar las operaciones.
+var cantidad18 = ++cantidad11 + cantidad12;
+var cantidad19 = cantidad13 + --cantidad13;
+var cantidad20 = cantidad18 * cantidad19;
+
+Console.WriteLine("El resultado de la cantidad15 es: " + cantidad16);
+
+
+// Constantes y valores magicos
+// Las constantes son variables cuyo valor no cambia durante la ejecución del programa.
+// Se usa la palabra clave const para declarar una constante.
+// Se recomienda usar constantes en lugar de valores mágicos.
+
+var cantidad21 = 1;
+cantidad21 = 2;
+
+const int miConstante = 7;
+
+const double FactorConversionKilometroPorHoraAMetrosPorSegundo = 3.6;
+
+// velocidad en kilometros por hora (KPH)
+// velocidad en metros por segundo (MPS)
+
+var velocidadKPH = 100;
+var velocidadMPS = velocidadKPH / FactorConversionKilometroPorHoraAMetrosPorSegundo;
+
+
+// Advertencia y Errores en C#
+// Control + . para corregir errores.
+// Evitar advertencia, Ctrl + . y seleccionar la opción: "Suprimir advertencia en el archivo".
+// #pragma warning disable CS0219
+// Lo que este dentro de #pragma warning disable CS0219 y #pragma warning restore CS0219 no generara advertencias.
+// #pragma warning restore CS0219
+
+// Ser extricto con las advertencias.
+// Ctrl + . = Suppress or Configure issues. = Configure CS0219. = Error.
+
+
+// Resumen
+// Las sentencias de alto nivel permiten archivos de código más limpios y fáciles de leer.
+// Solo se permite un archivo de alto nivel por programa.
+// Las variables se usan para almacenar información en la memoria de la computadora.
+// Los tipos de datos se usan para especificar el tipo de información que se va a almacenar en una variable.
+// Las variables de tipo de valor almacenan el valor directamente en la memoria.
+// Las variables de tipo de referencia almacenan una referencia a la memoria donde se encuentra el valor.
+// Los tipos de referencia pueden ser nulos.
+// Nulificables se usan para almacenar valores nulos.
+// Expresiones void no devuelven ningún valor.
+// Las constantes son variables cuyo valor no cambia durante la ejecución del programa.
+
+
+
